21f52418bdc8a05f458b489e6c41a8582953f759
tasynguye3894 <admin@ADMINs-MacBook-Pro.local>
2019-07-15 08:56:14 +0700

    fix deploy shell script

--- deploy.example.sh
+++ deploy.example.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-USER=root
-APP_NAME=TasyNuxt
-GIT_REMOTE=origin
-GET_BRANCH=master
+USER=
+APP_DIR=
+APP_NAME=
+GIT_REMOTE=
+GET_BRANCH=
 
 sudo su - $USER << EOF
+    cd $APP_DIR
     git status
     git pull $GIT_REMOTE $GET_BRANCH
+    npm run build
+    pm2 restart $TasyNuxt
 EOF
