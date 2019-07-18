fbcb23fe4d892da4a9a19b138f8a26c5316e0e02
tasynguye3894 <admin@ADMINs-MacBook-Pro.local>
2019-07-18 18:00:14 +0700

    Add new component for category

---
+++ components/template/Category.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flext w-1/3 border m-1 px-3 py-3">
+        <nuxt-link class="block text-lg font-bold hover:text-teal-500" :to="{name: 'categories-id', params: {id: category.id} }">
+            {{ category.name }}
+        </nuxt-link>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        category: {
+            required: true
+        }
+    }
+}
+</script>
--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <ul>
-            <li :key="category.id" v-for="category in categories">
-                <nuxt-link :to="{name: 'categories-id', params: {id: category.id} }">
-                    {{ category.name }}
-                </nuxt-link>
-            </li>
-        </ul>
+    <div class="w-full flex">
+        <Category v-bind:category="category" :key="category.id" v-for="category in categories" />
     </div>
 </template>
 <script>
 import categorySerivce from '~/service/category.service'
+import Category from '~/components/template/Category'
 
 export default {
     head() {
         return {
             title: 'Category | Tasy Book'
         }
+    },
+    components: {
+        Category
     },
     async asyncData({params, $axios, redirect}) {
         try {
